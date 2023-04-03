db1b255623cc20c1bc739f48f56afe1102d28b31
aravindsak <aravindk.ark@gmail.com>
2023-04-03 22:53:57 +0530

    spring-rest-api

---
+++ src/main/java/com/restapi/ApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
