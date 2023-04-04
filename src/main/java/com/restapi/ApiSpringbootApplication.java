b1f769dce51482a76baea5964284172a8595a745
aravindsak <aravindk.ark@gmail.com>
2023-04-04 11:17:27 +0530

    test-api

--- src/main/java/com/restapi/ApiSpringbootApplication.java
+++ src/main/java/com/restapi/ApiSpringbootApplication.java
@@ -2,9 +2,17 @@ package com.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiSpringbootApplication {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "springboot rest-api" ;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSpringbootApplication.class, args);
