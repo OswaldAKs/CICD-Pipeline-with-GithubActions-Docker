44a40ae29c8171989b48e67998763e9d006c822d
aravindsak <aravindk.ark@gmail.com>
2023-04-04 12:07:41 +0530

    controller-class for customer

---
+++ src/main/java/com/restapi/CustomersController.java
@@ -0,0 +1,26 @@
+package com.restapi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomersController {
+
+    @GetMapping
+    public String getCustomer(){
+        return "http GET request to get the customer details";
+    }
+    @PostMapping
+    public String createCustomer(){
+        return "http POST request to create the customer";
+    }
+    @PutMapping
+    public String updateCustomer(){
+        return "http GET request to update the customer details";
+    }
+    @DeleteMapping
+    public String deleteCustomer(){
+        return "http GET request to delete the customer";
+    }
+
+}
